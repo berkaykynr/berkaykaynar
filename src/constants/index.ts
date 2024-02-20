887102714d8732edad8e1aa2c4038394ee18a12a
berkay <berkay.kaynar65@gmail.com>
2024-02-20 16:19:40 +0300

    colors changed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,16 @@
 export const APP_NAME = "Berkay Kaynar";
-export const THEME_COLOR_BOLDER = "#190482";
-export const THEME_COLOR_BOLD = "#7752FE";
-export const THEME_COLOR_NORMAL = "#8E8FFA";
-export const THEME_COLOR_THIN = "#C2D9FF";
+// export const THEME_COLOR_BOLDER = "#190482";
+// export const THEME_COLOR_BOLD = "#7752FE";
+// export const THEME_COLOR_NORMAL = "#8E8FFA";
+// export const THEME_COLOR_THIN = "#C2D9FF";
+
+export const THEME_COLOR_BOLDER = "#048265";
+export const THEME_COLOR_BOLD = "#52fee1";
+export const THEME_COLOR_NORMAL = "#8efae8";
+export const THEME_COLOR_THIN = "#c2fff7";
+
+// $theme-color-bolder: #048265;
+// $theme-color-bold: #52fee1;
+// $theme-color-normal: #8efae8;
+// $theme-color-thin: #c2fff7;
+// $border-color: rgb(220, 220, 220);
