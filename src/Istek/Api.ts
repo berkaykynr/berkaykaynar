f2c3b940b68e4008a22e376eeebc7a5d0a8fa58c
berkay <berkay@berkay-MacBook-Air.local>
2024-10-15 00:15:28 +0300

    content type

--- src/Istek/Api.ts
+++ src/Istek/Api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 export async function getProductsApi(){
-   return await axios.get("http://209.38.216.132:5555/products") 
+   return await axios.get("http://209.38.216.132:5555/products",{
+    headers:{
+      'Content-Type': 'application/json',
+    }
+   }) 
 }
 
 export async function setProductsApi(prod:any) {
